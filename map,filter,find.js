1bfc543212454e86c33e2f392d25227d1ba9fb2a
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-08-20 23:26:49 +0600

    N

---
+++ map,filter,find.js
@@ -0,0 +1,9 @@
+const numbers = [5,4,87,54,5,8];
+const sqr = [];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    console.log(result);
+    sqr.push(result);
+    
+}
