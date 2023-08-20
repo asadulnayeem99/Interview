904ae4028e95ab538b230b050152e47972f7aba2
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-08-20 23:34:27 +0600

    Update

--- map,filter,find.js
+++ map,filter,find.js
@@ -1,9 +1,19 @@
-const numbers = [5,4,87,54,5,8];
+const numbers = [5, 4, 87, 54, 5, 8];
 const sqr = [];
 for (let i = 0; i < numbers.length; i++) {
-    const element = numbers[i];
-    const result = element * element;
-    console.log(result);
-    sqr.push(result);
-    
+  const element = numbers[i];
+  const result = element * element;
+//   console.log(result);
+  sqr.push(result);
 }
+console.log(sqr);
+
+// by map
+const result = numbers.map(function (element) {
+  return element * element;
+});
+console.log(result);
+
+//es6
+const res = numbers.map(x=>x*x);
+console.log(res);
